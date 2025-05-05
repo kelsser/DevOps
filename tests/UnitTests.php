b0996a13108bb3660afc3703051538c51334fb5d
Kelsser <kelsser992009@gmail.com>
2025-05-05 12:01:52 -0300

    Updated CI to include simples unit tests

---
+++ tests/UnitTests.php
@@ -0,0 +1,30 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class UnitTests extends TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testOnePlusOneEqualsTwo()
+    {
+        $this->assertEquals(2, 1 + 1);
+    }
+
+    public function testStringIsString()
+    {
+        $this->assertIsString("hello");
+    }
+
+    public function testArrayHasArray()
+    {
+        $this->assertArrayHasKey('key', ['key' => 'value']);
+    }
+
+    public function testValueIsInArray()
+    {
+        $this->assertContains(3, [1, 2, 3]);
+    }
+}
